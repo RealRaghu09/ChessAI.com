8bd70258a932ce7e8c664c8b38ef172b6de4118a
RaghuNandan <raghu.m2k5@gmail.com>
2026-06-29 19:01:13 +0530

    added the auth part and hash part

---
+++ backend/auth/password.py
@@ -0,0 +1,17 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str) -> str:
+    '''
+    Hash a password
+    '''
+    return pwd_context.hash(password)
+
+
+def verify_password(plain: str, hashed: str) -> bool:
+    '''
+    Verify a password
+    '''
+    return pwd_context.verify(plain, hashed)
